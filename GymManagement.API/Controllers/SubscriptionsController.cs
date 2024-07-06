5990d06c32a3eb499a7aeeddf809f3a9e4c3a84b
ArteMaxL <artemiolucero@gmail.com>
2024-07-06 15:09:01 -0300

    Create Subscription Request

---
+++ GymManagement.API/Controllers/SubscriptionsController.cs
@@ -0,0 +1,15 @@
+﻿using GymManagement.Contracts.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagement.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SubscriptionsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateSubscription(CreateSubscriptionRequest request)
+    {
+        return Ok(request);
+    }
+}
--- GymManagement.Contracts/Subscriptions/SubscriptionResponse.cs
+++ GymManagement.Contracts/Subscriptions/SubscriptionResponse.cs
@@ -1,3 +1,6 @@
 ﻿namespace GymManagement.Contracts.Subscriptions;
 
-public record SubscriptionResponse(Guid Id, SubscriptionType SubscriptionType);
+public record SubscriptionResponse(
+    Guid Id,
+    SubscriptionType SubscriptionType
+    );
