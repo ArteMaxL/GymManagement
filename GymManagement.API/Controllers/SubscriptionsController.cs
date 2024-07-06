0868ca3e9bab1c842cd9500a7a5ac3fb3599fd54
ArteMaxL <artemiolucero@gmail.com>
2024-07-06 15:45:03 -0300

    Added Subscriptions Service

--- GymManagement.API/Controllers/SubscriptionsController.cs
+++ GymManagement.API/Controllers/SubscriptionsController.cs
@@ -1,4 +1,5 @@
-﻿using GymManagement.Contracts.Subscriptions;
+﻿using GymManagement.Application.Services;
+using GymManagement.Contracts.Subscriptions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagement.API.Controllers;
@@ -7,9 +8,24 @@ namespace GymManagement.API.Controllers;
 [ApiController]
 public class SubscriptionsController : ControllerBase
 {
+    private readonly ISubscriptionsService _subscriptionsService;
+
+    public SubscriptionsController(ISubscriptionsService subscriptionsService)
+    {
+        _subscriptionsService = subscriptionsService;
+    }
+
     [HttpPost]
     public IActionResult CreateSubscription(CreateSubscriptionRequest request)
     {
-        return Ok(request);
+        var subscriptionId = _subscriptionsService.CreateSubscription(
+            request.SubscriptionType.ToString(),
+            request.AdminGuid);
+
+        var response = new SubscriptionResponse(
+            subscriptionId, 
+            request.SubscriptionType);
+
+        return Ok(response);
     }
 }
